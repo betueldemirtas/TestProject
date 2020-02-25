ffe109dd19f058587a6b65a87e5ac3ddec797f2a
betueldemirtas <61318608+betueldemirtas@users.noreply.github.com>
2020-02-25 11:11:52 +0100

    Kommentar hinzugefügt

--- Test/src/Palindrome.java
+++ Test/src/Palindrome.java
@@ -15,7 +15,7 @@ public static void main(String[] args) {
 	}
 	
 	public static boolean isPalindrome(String word) {
-		
+		// test
 		String replaceString = word.replaceAll("[ .,:;!?]", "");
 		
 		word = replaceString.toUpperCase(); 
